529f14773cbe947151fe1d3ff1b72aeed7d7ea79
AmmaR099-ctrl <muhammadammar1786q@gmail.com>
2026-08-01 07:14:37 +0400

    payroll system

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ I'm Muhammad Ammar. I started learning python during my first summer break becau
 ## Progress Log
 | Day | Topic | Key Concepts | Link |
 |-----|-------|---------------|------|
-| 02  | Vowel counter | strings, loops | [day01](./day01_vowel_counter) |
-| 03  | Registration system | input validation, while loops | [day02](./day02_registration_system) |
-| 04  | Shopping cart | OOP, classmethod/staticmethod | [day03](./day03_shopping_cart) |
+| 02  | Vowel counter | strings, loops | [day01](https://github.com/AmmaR099-ctrl/learning-python/blob/main/practice_day02.py) |
+| 03  | Registration system | input validation, while loops | [day02](https://github.com/AmmaR099-ctrl/learning-python/blob/main/practice_day03.py) |
+| 04  | Shopping cart | OOP, classmethod/staticmethod | [day03](https://github.com/AmmaR099-ctrl/learning-python/blob/main/practice_day04.py) |
--- practice_day05.py
+++ practice_day05.py
@@ -24,27 +24,24 @@ class Employee:
         print("ID: ",self._ID)
         print("Name: ",self._name)
         print("Basic: ",self._salary)
-        print("Extra time: ",self.__extra_time)
         print("Total salary: ",self._total_salary)
+        print("Extra time: ",self.__extra_time)
         print("Overtime rate: ",Employee.__overtime_rate)
 
-class manager(Employee):
+class Manager(Employee):
     def __init__(self, Id, name, salary):
         super().__init__(Id, name, salary)
         self.__bonus=0
     def add_bonus(self,bonus):
         self.__bonus+=bonus
     def set_total_salary(self):
-        self._total_salary=self._salary+self.__bonus-(self._salary*(manager._tax/100))
+        self._total_salary=self._salary+self.__bonus-(self._salary*(Manager._tax/100))
     def display(self):
-        print("ID: ",self._ID)
-        print("Name: ",self._name)
-        print("Basic: ",self._salary)
-        print("Total salary: ",self._total_salary)
+        super().display()
         print("bonus: ",self.__bonus)
 e1=Employee(4398,"ALi",70000)
 e2=Employee(3893,"Ans",72000)
-m=manager(10034,"Ahmad",120000)
+m=Manager(10034,"Ahmad",120000)
 Employee.overtime_rate(500)
 Employee.set_tax(7)
 e1.extra_time(16)
@@ -56,7 +53,7 @@ print("Employee 1 data: ")
 e1.display()
 print("Employee 2 data:\n")
 e2.display()
-manager.set_tax(5)
+Manager.set_tax(5)
 m.add_bonus(22000)
 m.set_total_salary()
 print("Manager data: ")
