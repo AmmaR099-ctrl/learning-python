d5b0166e57cf37ca4d78e642c66994cf60dbd07b
AmmaR099-ctrl <muhammadammar1786q@gmail.com>
2026-08-07 19:45:43 +0400

    practice day 08 added more functions in library management system

--- practice_day07/library.py
+++ practice_day07/library.py
@@ -22,52 +22,105 @@ class member:
     @staticmethod
     def borrow_book():
         Id = input("what is your ID: ")
-        with open("member.json", "r") as f:
-            members = json.load(f)
-
-        if Id not in members:
-            print("Invalid Member ID!")
-            return
-
-        print("name: ", members[Id][0], "\nborrowed books: ", members[Id][1])
-        
-        if len(members[Id][1]) < 5:
-            i = input("Enter 'y' if you want to borrow book: ")
-            if i == 'y':
-                result = member.check_book()
-                 
-                if result is not None:
-                    code, name = result
-                    members[Id][1].append([code, name])
-                        
-                    with open("member.json", "w") as f:
-                        json.dump(members, f, indent=4)
-                    print("Book borrowed successfully!")
-            else:
+        try:
+            with open("member.json", "r") as f:
+                members = json.load(f)
+        except(FileNotFoundError,json.JSONDecodeError):
+            print("file missing!")
+            return None
+        else:
+            if Id not in members:
+                print("Invalid Member ID!")
                 return
+
+            print("name: ", members[Id][0], "\nborrowed books: ", members[Id][1])
+            
+            if len(members[Id][1]) < 5:
+                i = input("Enter 'y' if you want to borrow book: ")
+                if i == 'y':
+                    result = member.check_book()
+                    
+                    if result is not None:
+                        code, name = result
+                        members[Id][1].append([code, name])
+                            
+                        with open("member.json", "w") as f:
+                            json.dump(members, f, indent=4)
+                        print("Book borrowed successfully!")
+                else:
+                    return
                         
     @staticmethod    
     def check_book():
         code = input("code of book: ")
+        try:
+            with open("books.json", "r") as f:
+                books = json.load(f)
+        except(FileNotFoundError,json.JSONDecodeError):
+            print("file missing!")
+            return None
+        else:        
+            if code not in books:
+                print("Book code not found!")
+                return None
+
+            print("available books: ", books[code][3])
         
-        with open("books.json", "r") as f:
-            books = json.load(f)
             
-        if code not in books:
-            print("Book code not found!")
+            if books[code][3] >= 1:
+                books[code][3] -= 1
+                with open('books.json', 'w') as f:
+                    json.dump(books, f, indent=4)
+                return code, books[code][0]
+            else:
+                print("Not available!")
+            
+                return None
+    @staticmethod
+    def return_book():
+        Id=input("what is your Id? ")
+        try:
+            with open('member.json','r') as f:
+                data=json.load(f)
+        except(FileNotFoundError,json.JSONDecodeError):
+            print ("File missing!")
             return None
-
-        print("available books: ", books[code][3])
-       
-        
-        if books[code][3] >= 1:
-            books[code][3] -= 1
-            with open('books.json', 'w') as f:
-                json.dump(books, f, indent=4)
-            return code, books[code][0]
         else:
-            print("Not available!")
-          
+            if Id not in data:
+                print("Invalid ID!")
+                return None
+        code=input("Enter code of book: ")
+        try:
+            with open ("books.json","r") as f:
+                books=json.load(f)
+        except(FileNotFoundError,json.JSONDecodeError):
+            print ("file misssing!")
             return None
+        else:
+            if code not in books:
+                print("Incorrect code")
+      
+        for book in data[Id][1]:
+            if book[0]==code:
+                data[Id][1].remove(book)
+                break
+        books[code][3]+=1
+        with open("member.json","w") as f:
+            json.dump(data,f,indent=4)
+        with open("books.json","w") as f:
+            json.dump(books,f,indent=4)
+            
+# member.borrow_book()
+while True:
+    c=int(input("---Main menu---\n1.borrow book\n2.return book\n3.main menu\n4.Exit\npress choice number: "))
+    if c==1:
+        member.borrow_book()
+    elif c==2:
+        member.return_book()
+    elif c==3:
+        continue
+    elif c==4:
+        break
+    else:
+        print("Incorrect choice! try again.")
 
-member.borrow_book()
